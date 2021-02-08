b6128158be0e777c0b5a9a3b0d0190b07a53376e
Muhammad Saad <50722220+msaad1311@users.noreply.github.com>
2021-02-08 12:12:45 -0500

    Update Practice-2.py

--- Practice-2.py
+++ Practice-2.py
@@ -17,7 +17,7 @@ class Book():
         self.author = author
         self.publisher = publisher
         self.pages = pages
-        self.price = price
+        self._price = price
         self.copies = copies
     def display(self):
         print('ISBN: \t', self.isbn)
@@ -38,12 +38,37 @@ class Book():
         else:
             print('The book is out of stock')    
     
+    @property
+    def price(self):
+        return self._price
+    
+    @price.setter
+    def price(self,new_price):
+        if self.new_price<50 | self.new_price>1000:      
+            raise ValueError('The price should be between 50 and 1000')     
+        else:
+            self_price = new_price
+       
+class Fraction():
+    def __init__(self,nr,dr=1):
+        self.nr = nr
+        self.dr = dr
+       
+
 
 book1 = Book('957-4-36-547417-1', 'Learn Physics','Stephen', 'CBC', 350, 200,0)
 book2 = Book('652-6-86-748413-3', 'Learn Chemistry','Jack', 'CBC', 400, 220,20)
 book3 = Book('957-7-39-347216-2', 'Learn Maths','John', 'XYZ', 500, 300,5)
 book4 = Book('957-7-39-347216-2', 'Learn Biology','Jack', 'XYZ', 400, 200,6)
 
-print(f'The number of books are {book1.copies}')
-book1.sell()
-print(f'The number of books after sell are {book1.copies}')
+# print(f'The number of books are {book1.copies}')
+# book1.sell()
+# print(f'The number of books after sell are {book1.copies}')
+
+# Answering the question 4
+
+books = [book1,book2,book3,book4]
+
+# print([books[i].display() for i in range(len(books))])
+titles = [book.title for book in books if book.author=='Jack']
+print(titles)
