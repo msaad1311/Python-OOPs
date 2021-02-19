f04c9d570b44799ff92166359c3e958191d9a3b8
Muhammad Saad <50722220+msaad1311@users.noreply.github.com>
2021-02-19 12:57:32 -0500

    Create Practice-3.py

---
+++ Practice-3.py
@@ -0,0 +1,28 @@
+class Fraction():
+    def __init__(self,nr,dr=1):
+        self.nr = nr
+        self.dr = dr
+        if self.dr<0:
+            self.nr*=-1
+            self.dr*=-1
+    def show(self):
+        print(f'{self.nr}/{self.dr}')
+    def multiply(self,other):
+        if isinstance(other,int):
+            other = Fraction(other)
+        return Fraction(self.nr*other.nr,self.dr*other.dr)
+    def add(self,other):
+        if isinstance(other,int):
+            other = Fraction(other)
+        return Fraction(self.nr * other.dr + self.dr * other.nr,self.dr*other.dr)
+    @staticmethod
+    def hcf(x,y):
+        x=abs(x)
+        y=abs(y)
+        smaller = y if x>y else x
+        s = smaller
+        while s>0:
+            if x%s==0 and y%s==0:
+                break
+            s-=1
+        return s
